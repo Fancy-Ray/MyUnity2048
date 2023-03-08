055f35ba23f33bccf5b762b97f04890dab32935e
Fancy-Ray <1273117205@qq.com>
2023-03-08 23:38:53 +0800

    nearly complete

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using System;
+
 namespace MainNameSpace
 {
     public class Main : MonoBehaviour
     {
-        public GameObject element;
-        Vector3[] coordinates = {
+        public  GameObject element;
+        public static Vector3[] coordinates = {
             new Vector3(-3, 3), new Vector3(-1, 3), new Vector3(1, 3), new Vector3(3,3) ,
             new Vector3(-3, 1), new Vector3(-1, 1), new Vector3(1, 1), new Vector3(3,1) ,
             new Vector3(-3, -1), new Vector3(-1, -1), new Vector3(1, -1), new Vector3(3,-1) ,
@@ -17,42 +19,99 @@ namespace MainNameSpace
         // Start is called before the first frame update
         void Start()
         {
-            Instantiate(element, coordinates[0], this.transform.rotation);
+            Instantiate(element, RandomIdleCoordinate(), this.transform.rotation);
         }
 
         // Update is called once per frame
         void Update()
         {
+            if(gameObject.transform.Find("back").tag == "Moving")
+                Invoke("ChangeState", 1.5f);
             if (Input.GetKeyUp(KeyCode.UpArrow) ||
-                Input.GetKeyUp(KeyCode.DownArrow) ||
-                Input.GetKeyUp(KeyCode.RightArrow) ||
-                Input.GetKeyUp(KeyCode.LeftArrow)) {
-                if (IfObjectsStop()) {
-                    Instantiate(element, randomIdleCoordinate(), this.transform.rotation);
+    Input.GetKeyUp(KeyCode.DownArrow) ||
+    Input.GetKeyUp(KeyCode.RightArrow) ||
+    Input.GetKeyUp(KeyCode.LeftArrow))
+            {
+                gameObject.transform.Find("back").tag = "Moving";
+                if (IfObjectsStop())
+                {
+                    Instantiate(element, RandomIdleCoordinate(), this.transform.rotation);
                 }
             }
         }
+        void ChangeState() {
+            gameObject.transform.Find("back").tag = "Unmoving";
+        }
+        private void FixedUpdate()
+        {
+
+
+
+        }
         public static bool IfObjectsStop()
         {
-            Vector2 vector2 = new Vector2(0, 0);
-            foreach (var gO in GameObject.FindGameObjectsWithTag("Numbers")) {
-                if (gO.GetComponent<Rigidbody2D>().velocity != vector2)
+            foreach (var gO in GameObject.FindGameObjectsWithTag("Numbers"))
+            {
+                //float x = gO.GetComponent<Rigidbody2D>().velocity.x;
+                //float y = gO.GetComponent<Rigidbody2D>().velocity.y;
+                //if (x < -1 && x > 1 && y < -1 && y > 1)
+                //if(gO.GetComponent<Rigidbody2D>().constraints != RigidbodyConstraints2D.FreezeAll)
+
+                if (gO.GetComponent<Rigidbody2D>().velocity != Vector2.zero)
+
+                return false;
+            }
+            return true;
+        }
+        public static bool IfObjectsNearlyStop()
+        {
+            foreach (var gO in GameObject.FindGameObjectsWithTag("Numbers"))
+            {
+                float x = gO.GetComponent<Rigidbody2D>().velocity.x;
+                float y = gO.GetComponent<Rigidbody2D>().velocity.y;
+                if (x < -1 && x > 1 && y < -1 && y > 1)
                     return false;
             }
             return true;
         }
-        private Vector3 randomIdleCoordinate() {
-            foreach (var gO in GameObject.FindGameObjectsWithTag("Numbers")) {
-                foreach (var gOCoor in coordinates)
+        public static void OrganizePositions()
+        {
+            foreach (var gO in GameObject.FindGameObjectsWithTag("Numbers"))
+            {
+                float x = gO.GetComponent<Transform>().position.x;
+                float y = gO.GetComponent<Transform>().position.y;
+                foreach (var coordi in Main.coordinates)
                 {
-                    if (gO.GetComponent<Transform>().transform.position.Equals(gOCoor))
-                        //这里可以这样写，因为所有的坐标引用都是指向的Coordinates数组中的对象
-                        break;
-                    else
-                        return gOCoor;
+                    if (Math.Abs(x - coordi.x) < 1 && Math.Abs(y - coordi.y) < 1)
+                    {
+                        gO.GetComponent<Transform>().position = coordi;
+                        gO.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeRotation;
+                    }
                 }
             }
-            return new Vector3(0,0);
+        }
+        //private Vector3 RandomIdleCoordinate()
+        public static Vector3 RandomIdleCoordinate()
+        {
+            int index = 0;
+            //for (index=0; index < coordinates.Length; index++) 
+            while (index < coordinates.Length)
+            {
+                Labell:
+                foreach (var gO in GameObject.FindGameObjectsWithTag("Numbers"))
+                {
+                    if (gO.GetComponent<Transform>().transform.position.x != coordinates[index].x || gO.GetComponent<Transform>().transform.position.y != coordinates[index].y)
+                        //这里可以这样写，因为所有的坐标引用都是指向的Coordinates数组中的对象
+                        ;
+                    else
+                    {
+                        index++;
+                        goto Labell;
+                    }
+                }
+                return coordinates[index];
+            }
+            return coordinates[index];
         }
 
     }
--- Assets/Scripts/element.cs
+++ Assets/Scripts/element.cs
@@ -3,51 +3,131 @@ using System.Collections.Generic;
 using UnityEngine;
 using MainNameSpace;
 using System;
+using TMPro;
 
 public class element : MonoBehaviour
 {
+    public GameObject element_;
     // Start is called before the first frame update
     void Start()
     {
-
+        Invoke("ForStart", 1.5f);
     }
-
+    private void FixedUpdate()
+    {
+    }
     // Update is called once per frame
+    void ForStart()
+    {
+        gameObject.GetComponent<BoxCollider2D>().enabled = true;
+        isCollided = false;
+}
+    void ChangeState()
+    {
+        gameObject.transform.Find("back").tag = "Unmoving";
+    }
     void Update()
     {
-        Vector2 vector2 = new Vector2(0,0);
-        if (Input.GetKeyUp(KeyCode.UpArrow)) {
+        if (gameObject.transform.Find("back").tag == "Moving")
+            Invoke("ChangeState", 1.5f);
+        Vector2 vector2 = new Vector2(0, 0);
+
+        float x = gameObject.GetComponent<Rigidbody2D>().velocity.x;
+        float y = gameObject.GetComponent<Rigidbody2D>().velocity.y;
+
+        float x_ = gameObject.GetComponent<Transform>().position.x;
+        float y_ = gameObject.GetComponent<Transform>().position.y;
+
+        if (gameObject.transform.Find("back").tag == "Unmoving")
+            if (x > -1 && x < 1 && y > -1 && y < 1)
+                foreach (var coordi in Main.coordinates)
+                {
+                    if (Math.Abs(x_ - coordi.x) < 1 && Math.Abs(y_ - coordi.y) < 1)
+                    {
+                        gameObject.GetComponent<Transform>().position = coordi;
+                        gameObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
+                    }
+                }
+
+        //检测是否控制方块移动位置
+        if (Input.GetKeyUp(KeyCode.UpArrow))
+        {
             //gameObject.transform.position = new Vector2(gameObject.transform.position.x + 1, gameObject.transform.position.y);
             if (Main.IfObjectsStop())
-                gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0,10));
+            {
+                gameObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeRotation;
+                vector2 = new Vector2(0, 10);
+                gameObject.GetComponent<Rigidbody2D>().AddForce(vector2);
+                gameObject.transform.Find("back").tag = "Moving";
+
+            }
         }
         if (Input.GetKeyUp(KeyCode.DownArrow))
         {
             if (Main.IfObjectsStop())
-                gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, -10));
+            {
+                gameObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeRotation;
+                vector2 = new Vector2(0, -10);
+                gameObject.GetComponent<Rigidbody2D>().AddForce(vector2);
+                gameObject.transform.Find("back").tag = "Moving";
+            }
+
         }
         if (Input.GetKeyUp(KeyCode.RightArrow))
         {
             if (Main.IfObjectsStop())
-                gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(10, 0));
+            {
+                gameObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeRotation;
+                vector2 = new Vector2(10, 0);
+                gameObject.GetComponent<Rigidbody2D>().AddForce(vector2);
+                gameObject.transform.Find("back").tag = "Moving";
+            }
         }
         if (Input.GetKeyUp(KeyCode.LeftArrow))
         {
             if (Main.IfObjectsStop())
-                gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(-10, 0));
-        }
-
-        if (gameObject.GetComponent<Rigidbody2D>().velocity == vector2) {
-            gameObject.GetComponent<Transform>().transform.position.x = Math.Round(gameObject.GetComponent<Transform>().transform.position.x, 0);
+            {
+                gameObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeRotation;
+                vector2 = new Vector2(-10, 0);
+                gameObject.GetComponent<Rigidbody2D>().AddForce(vector2);
+                gameObject.transform.Find("back").tag = "Moving";
+            }
         }
     }
+    public bool isCollided = false;
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Vector2 vector2 = new Vector2(0, 0);
-        if (collision.gameObject.tag == "Numbers")
-        {
-            if (collision.gameObject.GetComponent<Rigidbody2D>().velocity == vector2)
-                gameObject.GetComponent<Rigidbody2D>().velocity = vector2;
-        }
+        if (collision.gameObject.transform.tag == "Numbers")
+            if (Convert.ToInt32(collision.gameObject.GetComponentInChildren<TMP_Text>().text) == Convert.ToInt32(gameObject.GetComponentInChildren<TMP_Text>().text))
+            {
+                if (!collision.gameObject.GetComponent<element>().isCollided)
+                {
+                    isCollided = true;
+                    GameObject gO = Instantiate(gameObject, collision.transform.position, this.transform.rotation);
+                    gO.GetComponent<element>().enabled = true;
+                    gO.GetComponentInChildren<TextMeshPro>().enabled = true;
+                    gO.GetComponentInChildren<TextMeshPro>().text =
+                        Convert.ToString(Convert.ToInt32(gameObject.GetComponentInChildren<TextMeshPro>().text) +
+                        Convert.ToInt32(collision.gameObject.GetComponentInChildren<TextMeshPro>().text));
+                    //gO.GetComponent<Rigidbody2D>().velocity = gameObject.GetComponent<Rigidbody2D>().velocity;
+                    Destroy(collision.gameObject);
+                    Destroy(gameObject);
+                }
+            }
+        float x = gameObject.GetComponent<Rigidbody2D>().velocity.x;
+        float y = gameObject.GetComponent<Rigidbody2D>().velocity.y;
+
+        float x_ = gameObject.GetComponent<Transform>().position.x;
+        float y_ = gameObject.GetComponent<Transform>().position.y;
+        if (collision.gameObject.transform.Find("back").tag == "Unmoving")
+            if (x > -5 && x < 5 && y > -5 && y < 5)
+                foreach (var coordi in Main.coordinates)
+                {
+                    if (Math.Abs(x_ - coordi.x) < 1 && Math.Abs(y_ - coordi.y) < 1)
+                    {
+                        gameObject.GetComponent<Transform>().position = coordi;
+                        gameObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
+                    }
+                }
     }
 }
